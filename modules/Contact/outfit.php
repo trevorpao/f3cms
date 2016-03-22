f9d1337d1931820b7313e6f51763487357b96fd8
Trevor Pao <shuaib25@gmail.com>
2016-03-22 08:02:31 +0800

    about and  contact sample

--- modules/Contact/outfit.php
+++ modules/Contact/outfit.php
@@ -9,15 +9,8 @@ class oContact extends Outfit
 
     function do_contact ($f3, $args)
     {
-        $cu = fPost::get_row('/contact', 'slug', " AND `status`='". fPost::ST_ON ."' ");
-
-        if (empty($cu)) {
-            f3()->error(404);
-        }
-
-        f3()->set('cu', $cu);
         $f3->set('act_link', 'contact');
 
-        parent::wrapper('contact.html', $cu['title'], '/contact');
+        parent::wrapper('contact.html', 'Contact me', '/contact');
     }
 }
