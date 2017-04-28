6802f512bf1c28aa46a3114a596d5d034934fba4
Trevor Pao <shuaib25@gmail.com>
2017-04-29 00:11:44 +0800

    add maligun

--- libs/Sender.php
+++ libs/Sender.php
@@ -1,8 +1,12 @@
 <?php
 namespace F3CMS;
 
+use Mailgun\Mailgun;
+
 class Sender extends Helper
 {
+    const MAILGUN_KEY = 'key-';
+    const MAILGUN_DOMAIN = 'domain.com';
 
     /**
      * adapter for SMTP
@@ -15,15 +19,19 @@ class Sender extends Helper
      */
     static function sendmail($subject, $content, $receiver)
     {
-        $to_address = ($receiver == "") ? f3()->get('inquiry_receiver') : $receiver;
+        $to_address = ($receiver == "") ? f3()->get('webmaster') : $receiver;
 
         $smtp = new \SMTP(f3()->get('smtp_host'), f3()->get('smtp_port'), 'SSL', f3()->get('smtp_account'), f3()->get('smtp_password'));
 
-        $smtp->set('From', '"' . f3()->get('smtp_name') . '" <' . f3()->get('smtp_account') . '>');
+        $subject = "=?UTF-8?B?" . base64_encode($subject) . "?=";
+        $from_name = "=?UTF-8?B?" . base64_encode(f3()->get('smtp_name')) . "?=";
+
+        $smtp->set('From', '"' . $from_name . '" <' . f3()->get('smtp_account') . '>');
         $smtp->set('To', '<' . $to_address . '>');
         $smtp->set('Subject', $subject);
         $smtp->set('Errors-to', '<' . f3()->get('smtp_account') . '>');
-        $smtp->set('Content-Type', 'text/html');
+        $smtp->set('bcc', '<skytzz@osha.gov.tw>');
+        $smtp->set('Content-Type', 'text/html;  charset=UTF-8');
 
         $sent = $smtp->send($content, TRUE);
 
@@ -48,7 +56,7 @@ class Sender extends Helper
      */
     static function mail($subject, $content, $receiver = "")
     {
-        $to_address = ($receiver == "") ? f3()->get('inquiry_receiver') : $receiver;
+        $to_address = ($receiver == "") ? f3()->get('webmaster') : $receiver;
 
         $from_address = f3()->get('smtp_account');
         $subject = "=?UTF-8?B?" . base64_encode($subject) . "?=";
@@ -57,9 +65,25 @@ class Sender extends Helper
         $headers.= "MIME-Version: 1.0\r\n";
         $headers.= "From:" . $from_address . "(" . $from_name . ")\r\n";
         if ($to_address != $from_address) {
-            $headers.= "bcc:" . f3()->get('inquiry_receiver') . "\r\n";
+            $headers.= "bcc:" . f3()->get('webmaster') . "\r\n";
         }
         mail($to_address, $subject, $content, $headers);
         return 'Done';
+    }
+
+    public function send($receiver, $subject, $body)
+    {
+        $to_address = ($receiver == "") ? f3()->get('webmaster') : $receiver;
+        $mgClient = new Mailgun(self::MAILGUN_KEY);
+
+        $result = $mgClient->sendMessage(self::MAILGUN_DOMAIN, array(
+            'from'    => 'Web Service<'. f3()->get('webmaster') .'>',
+            'to'      => $to_address,
+            'bcc'     => f3()->get('webmaster'),
+            'subject' => $subject,
+            'html'    => $body
+        ));
+
+        return $result;
     }
 }
