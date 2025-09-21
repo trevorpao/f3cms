343b78da03ff746e426e388e7264293832b60389
Trevor Pao <shuaib25@gmail.com>
2025-09-21 12:21:09 +0800

    add test mode & update composer

--- cli.php
+++ cli.php
@@ -9,20 +9,21 @@ $f3 = \Base::instance();
 // config
 require './config.php';
 
+$logger = new \Log('crontab.log');
+$f3->set('cliLogger', $logger);
+
+$logger->write('Info - 新排程');
+
+if ($f3->get('crontabhost') != gethostname()) {
+    $logger->write('Error - 本機不能執行排程');
+    exit();
+}
+
 $f3->set('opts', \F3CMS\fOption::load('', 'Preload'));
 
 // Define routes
 $f3->route('GET /', function ($f3, $args) {
-    echo '**F3CMS CLI**';
+    echo PHP_EOL.'**'. $f3->get('site_title') .' CLI**'.PHP_EOL.PHP_EOL;
 });
 
-$f3->route('GET /demo', function ($f3, $args) {
-    $logger = new \Log('move.log');
-    $worker = new \F3CMS\Worker('\F3CMS\rPost', 'get', $logger);
-
-    $worker->startWorker(
-        ['9P1917', '9F0276', '7F7862', '6Y7912', '4Q7199', 'S2346', '32173J', '62173B', '23471Q'], 'Queue'
-    );
-});
-
-$f3->run();
+$f3->route('GET /@freq/@tally', '\F3CMS\rCrontab->do_job');
--- index.php
+++ index.php
@@ -41,9 +41,36 @@ if ($f3->get('DEBUG') == 0) {
         if ($isAjax) {
             \F3CMS\Reaction::_return(404);
         } else {
-            \F3CMS\Outfit::wrapper('error.html', 'Not Found', '/404');
+            $error = $f3->get('ERROR');
+            _dzv('ERROR', [
+                'code' => $error['code'],
+                'text' => $error['text'],
+            ]);
+            \F3CMS\Outfit::render('error.twig', 'Not Found', '/404');
         }
     });
+}
+
+$_SERVER['HTTP_ORIGIN'] = (empty($_SERVER['HTTP_ORIGIN'])) ? null : $_SERVER['HTTP_ORIGIN'];
+$_SERVER['HTTP_USER_AGENT'] = (empty($_SERVER['HTTP_USER_AGENT'])) ? '' : $_SERVER['HTTP_USER_AGENT'];
+
+// if ($_SERVER['SERVER_NAME'] !== 'loc.f3cms.com') {
+if ($f3->get('APP_ENV') == 'qa' && $_SERVER['SERVER_NAME'] !== 'loc.f3cms.com') {
+    $testing = $f3->get('SESSION.qa');
+    if (!$testing || !$f3->exists('GET.qa')) {
+        $isLegalQA = $f3->get('feVersion') == $f3->get('SESSION.qa')
+            || $f3->get('feVersion') == $f3->get('GET.qa')
+            || strpos($_SERVER['REQUEST_URI'], '/slack/action') === 0; // for 3th party api
+
+        if ($isLegalQA) {
+            $f3->set('SESSION.qa', $f3->get('feVersion'));
+        } else {
+            $f3->set('SESSION.qa', 0);
+            $f3->reroute('https://sense-info.co'); // . $_SERVER['REQUEST_URI']
+            exit();
+        }
+    }
+    // $f3->set('feVersion', date('YmdHis'));
 }
 
 $f3->set('opts', \F3CMS\fOption::load('', 'Preload'));
